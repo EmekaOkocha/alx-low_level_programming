ff5b0adb20cfb61a53bd8d0017bd8329c075a591
EmekaOkocha <okocha007@gmail.com>
2023-08-14 07:02:27 -0700

    printf bug fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,14 +13,14 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf(n, "is positive");
+		printf("%d" n, "is positive");
 	}
 	else if(n = 0)
         {
-                printf(n, "is zero");
+                printf("%d" n, "is zero");
         }
 	else
-	       {	printf(n, "is positive");
+	       {	printf("%d" n, "is positive");
 	       }
 	return 0;
 }

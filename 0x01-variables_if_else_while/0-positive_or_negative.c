8671223caa8b6514a80472d16c9fdd3ab8a22909
EmekaOkocha <okocha007@gmail.com>
2023-08-14 18:44:32 -0700

    removed white spaces in comments and added parenthesis in code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-/* 
+/*
  * main - A program that prints the sign of n
  * Return: 0
- */
+*/
 int main(void)
 {
 	int n;
@@ -13,15 +13,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if(n > 0)
-	
+	{
 		printf("%d is positive\n", n);
-	
+	}
 	else if(n == 0)
-        
+	{       
                 printf("%d is zero\n", n);
-        
+	}
 	else
-	      	printf("%d is negative\n", n);
-	      
-	return 0;
+	{
+		printf("%d is negative\n", n);
+	}
+				}
+	return (0);
 }

16482a6f72172a7fce8da1ac4af498d195c2a4e1
EmekaOkocha <okocha007@gmail.com>
2023-08-14 18:50:59 -0700

    corrected betty whitespaces and if statement parenthesis

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /*
  * main - A program that prints the sign of n
  * Return: 0
@@ -12,11 +11,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
+	if (n > 0)
 	
 		printf("%d is positive\n", n);
 	
-	else if(n == 0)
+	else if (n == 0)
 	       
                 printf("%d is zero\n", n);
 	

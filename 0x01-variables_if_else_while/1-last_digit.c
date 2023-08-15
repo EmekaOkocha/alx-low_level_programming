c200052def070e7fd2720d83c5dfd69fa7888123
EmekaOkocha <okocha007@gmail.com>
2023-08-14 22:07:39 -0700

    greater than 5

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,6 +11,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+	if (n > 5)
+		printf("%d last digit of", n "is", n "and is greater than 5\n")
+			return (0);
 }
